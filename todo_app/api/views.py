32e64b9eea791291ceadcdb97e28f533143dfd5c
zalkier <zalkier@hotmail.com>
2021-01-30 11:27:12 +0000

    finished preparing api

---
+++ todo_app/api/views.py
@@ -0,0 +1,13 @@
+from todos import models
+from .serializers import TodoSerializer
+from rest_framework import generics
+
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
